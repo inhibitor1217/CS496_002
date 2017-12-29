ff3d8635456e54ce0de65d770ff3a2dc9cd1530b
inhibitor1217 <cytops1217@gmail.com>
2017-12-29 16:12:13 +0900

    v0
    2017/12/29 16:12
    
    - Added 3 Tabs

---
+++ app/src/main/java/com/example/user/cs496_002/TabFragment2.java
@@ -0,0 +1,19 @@
+package com.example.user.cs496_002;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/user/cs496_002/TabPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.user.cs496_002;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragments = new ArrayList<>();
+    private final List<String> mFragmentTitles = new ArrayList<>();
+
+    public TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragments.add(fragment);
+        mFragmentTitles.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitles.get(position);
+    }
+
+}
